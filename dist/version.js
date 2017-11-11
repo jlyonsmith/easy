cbfe90b8897c9beb24193d3f50b2b4bf8a16966d
John Lyon-Smith <john@lyon-smith.org>
2017-11-11 13:01:57 -0800

    Version 1.1.1-20171111.0

--- dist/version.js
+++ dist/version.js
@@ -3,6 +3,6 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-const version = exports.version = '1.1.0';
-const fullVersion = exports.fullVersion = '1.1.0-20171111.0';
+const version = exports.version = '1.1.1';
+const fullVersion = exports.fullVersion = '1.1.1-20171111.0';
 //# sourceMappingURL=version.js.map
--- src/version.js
+++ src/version.js
@@ -1,2 +1,2 @@
-export const version = '1.1.0'
-export const fullVersion = '1.1.0-20171111.0'
+export const version = '1.1.1'
+export const fullVersion = '1.1.1-20171111.0'
