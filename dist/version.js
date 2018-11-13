3051b49e7ea40bf462872679ebb1f4f0abee3b5a
John Lyon-Smith <john@lyon-smith.org>
2018-11-12 16:17:46 -0800

    Version 2.5.1-20181112.0

--- dist/version.js
+++ dist/version.js
@@ -4,8 +4,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.fullVersion = exports.version = void 0;
-const version = '2.5.0';
+const version = '2.5.1';
 exports.version = version;
-const fullVersion = '2.5.0-20181112.0';
+const fullVersion = '2.5.1-20181112.0';
 exports.fullVersion = fullVersion;
 //# sourceMappingURL=version.js.map
