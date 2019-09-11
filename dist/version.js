8d304ef6f38b4ae9fcb4b75ade93f4acb7e0933e
John Lyon-Smith <john@lyon-smith.org>
2019-09-10 23:55:46 -0700

    Version 4.4.1+20190910.0

--- dist/version.js
+++ dist/version.js
@@ -4,8 +4,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.fullVersion = exports.shortVersion = void 0;
-const shortVersion = "4.4.0";
+const shortVersion = "4.4.1";
 exports.shortVersion = shortVersion;
-const fullVersion = "4.4.0+20190910.0";
+const fullVersion = "4.4.1+20190910.0";
 exports.fullVersion = fullVersion;
 //# sourceMappingURL=version.js.map
